0f4aeba345a52d3ea95a39d57e8fde861e29080d
anca6 <526100@student.saxion.nl>
2023-11-23 16:58:27 +0100

    player movement + camera control

---
+++ Startup URP/Assets/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 8f;
+
+    public Vector3 offset;
+
+    void Update()
+    {
+        if (target == null) return;
+
+        Vector3 desiredPosition = new Vector3(target.position.x + offset.x, target.position.y + offset.y, target.position.z + offset.z);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Startup URP/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    CustomActions input;
+
+    NavMeshAgent agent;
+    Animator animator;
+
+    [Header("Movement")]
+    [SerializeField] LayerMask clickableLayers;
+
+    float lookRotationSpeed = 5f;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        input = new CustomActions();
+        AssignInputs();
+    }
+
+    void AssignInputs()
+    {
+        input.Main.Move.performed += ctx => ClickToMove();
+    }
+
+    void ClickToMove()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, clickableLayers))
+        {
+            agent.destination = hit.point;
+        }
+    }
+
+    void OnEnable()
+    {
+        input.Enable();
+    }
+
+    void OnDisable()
+    {
+        input.Disable();
+    }
+
+    void Update()
+    {
+        FaceTarget();
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (agent.destination - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * lookRotationSpeed);
+    }
+
+    void SetAnimation()
+    {
+
+    }
+}
