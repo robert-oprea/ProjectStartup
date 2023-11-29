a30eb10fde815365cef3fb4b172300ed89acbf0c
morqp <mara.cosneanu@yahoo.com>
2023-11-29 10:14:53 +0100

    changes to drag and drop and start of database

--- Startup URP/Assets/ReadDictionary.cs
+++ Startup URP/Assets/ReadDictionary.cs
@@ -6,21 +6,70 @@ using System.IO;
 public class ReadDictionary : MonoBehaviour
 {
     public string filename;
+    public string[] lines;
+    public string[] words;
+
+    public string[][] allWords = new string[10][];
+    public int lineNr = 0;
+
+    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        string[] lines = File.ReadAllLines("Assets/"+filename);
+        lines = File.ReadAllLines("Assets/"+filename);
+        
+
         foreach (string line in lines)
         {
-            string[] words = line.Split(','); // Should work as long as there are no comma's in the Excel sheet
-            Debug.Log(words[0] + " is the Spanish word for " + words[1]);
+            words = line.Split(','); // Should work as long as there are no comma's in the Excel sheet  //Debug.Log(words[0] + " is the Spanish word for " + words[1]);
+
+            allWords[lineNr] = words;
+
+            Debug.Log(allWords[lineNr]);
+            //ask paul why words don t get assigned to this
+
+            lineNr++;
+
+
+
+            /*for (int i = 0; i <= words.Length; i++)
+            {
+                for (int j = 0; j <= lines.Length; j++)
+                {
+                    string[,] allWords = new string[words.Length, lines.Length];
+                    allWords[i, j] = lines[j];
+
+                }
+            }*/
+
+
         }
+
+        /*for (int i = 0; i <= lineNr; i++)
+        {
+            
+                Debug.Log(allWords[i] + "\n");
+
+            
+        }*/
+        
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
+
+
+
+
+
+
 }
